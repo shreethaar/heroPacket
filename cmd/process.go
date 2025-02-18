ad58c150eab190102e7028bdaebd2032d0c19a32
shreethaar <shreethaara380@gmail.com>
2025-02-19 07:36:54 +0800

    update with flag

--- cmd/process.go
+++ cmd/process.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-  //  "flag"
+    "flag"
     "fmt"
     "os"
     "strings"
@@ -23,24 +23,35 @@ Handling user input
 */
 
 
-
-
-
 func handleInput() {
+    pcapfile:=flag.String("r","","Path to a PCAP file (.pcap, .pcapng, .cap)")
+    flag.Parse()
+
+    if*pcapfile=="" {
+        fmt.Println("Usage: go run . -r <pcapfile>")
+        return
+    }
+
     if len(os.Args) < 2 {
         fmt.Println("Please enter a PCAP file path.")
         return 
     }
+    /*
     pcapfile := os.Args[1]
     if valid(pcapfile) {
         fmt.Println("Valid PCAP File:",pcapfile)
     } else {
         fmt.Println("Invalid PCAP File. Please provide a .pcap, .pcapng, or .cap file.")
     }
-}
+    */
 
- //   fmt.Println("PCAP file: ",pcapfile)
-   
+    if !valid(*pcapfile) {
+        fmt.Println("Invalid PCAP File. Please provide a .pcap, .pcapng, or .cap file.")
+        return
+    }
+    fmt.Println("Valid PCAP file: ",*pcapfile)
+}
+    
 func valid(filename string) bool {
     validExtensions:=[]string{".pcap",".pcapng",".cap"}
     for _,ext:=range validExtensions {
