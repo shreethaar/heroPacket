a5c00557efaa48d9823c0a59b39f4bf9d15814df
shreethaar <shreethaara380@gmail.com>
2025-03-12 05:42:19 +0800

    [jm61D5QtHAITc]

--- config/config.go
+++
@@ -1,9 +0,0 @@
-// If this file exists and contains CSRF configuration, remove it
-
-// Keep other configurations intact
-
-// Example:
-// Remove any lines like:
-// CSRFEnabled: true
-// or
-// EnableCSRF: true 
